95ce85b8510e2c0fb208de7aee18225fec065cc6
aramrw <aramitsa@gmail.com>
2023-11-02 16:43:44 -0700

    first commit

---
+++ dist/popup.js
@@ -0,0 +1,43 @@
+"use strict";
+const toggleButton = document.querySelector('button');
+// check local storage for the state of the toggle button
+const disabled = localStorage.getItem('disabled');
+if (disabled === 'true') {
+    toggleButton.classList.add('disabled');
+    toggleButton.textContent = 'OFF';
+    // send a message to the content script
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        const activeTab = tabs[0];
+        if (activeTab) {
+            chrome.tabs.sendMessage(activeTab.id, { message: 'disable' });
+        }
+    });
+}
+else {
+    toggleButton.classList.remove('disabled');
+    toggleButton.textContent = 'ON';
+    // send a message to the content script
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        const activeTab = tabs[0];
+        if (activeTab) {
+            chrome.tabs.sendMessage(activeTab.id, { message: 'enable' });
+        }
+    });
+}
+toggleButton.addEventListener("click", () => {
+    toggleButton.classList.toggle('disabled');
+    if (toggleButton.classList.contains('disabled')) {
+        toggleButton.textContent = 'OFF';
+        //save the state to local storage
+        localStorage.setItem('disabled', 'true');
+        // reload the window
+        chrome.tabs.reload();
+    }
+    else {
+        toggleButton.textContent = 'ON';
+        //save the state to local storage
+        localStorage.setItem('disabled', 'false');
+        // reload the window
+        chrome.tabs.reload();
+    }
+});
---
+++ src/contentscript.ts
@@ -0,0 +1,65 @@
+const handleYoutubeLoaded = () => {
+    const youtubeLogo = document.querySelector('#logo');
+    const videoLinks = Array.from(document.querySelectorAll('#video-title')) as HTMLElement[];
+
+    if (youtubeLogo) {
+        for (const link of videoLinks) {
+            const videoTitle = link.textContent; // Get the text content of the link
+            const richGridRow = link.closest('ytd-rich-grid-row');
+            const sectionRenderer = link.closest('ytd-video-renderer');
+
+            // remove all shorts videos
+            const shortsRenderer = link.closest('ytd-reel-shelf-renderer');
+            if (shortsRenderer) {
+                shortsRenderer?.remove();
+            }
+
+            if (videoTitle) {
+                if (!containsJapaneseCharacter(videoTitle)) {
+                    // console.log('Removing link: ', videoTitle);
+                    richGridRow?.remove();
+                    if (sectionRenderer) {
+                        sectionRenderer?.remove();
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+function containsJapaneseCharacter(text: string) {
+    // Check if the text contains any character from the Unicode ranges for Japanese scripts
+    return /[一-龯ぁ-んァ-ヶ]/.test(text);
+}
+
+
+// get the state of the toggle button from local storage
+if (localStorage.getItem('disabled') === 'true') {
+    localStorage.setItem('disabled', 'true');
+    const mutationObserver = new MutationObserver(handleYoutubeLoaded);
+    mutationObserver.observe(document, {
+        childList: true,
+        subtree: true
+    });
+} else {
+    // do nothing since the program is disabled
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+
+    if (request.message === "enable") {
+        localStorage.setItem('disabled', 'false');
+    } else {
+        localStorage.setItem('disabled', 'true');
+        // do nothings since the program is disabled
+    }
+})
+
+
+
+
+
+
+
+
